966aa5d77909c6b93eb5ea53c92b2de4d8f16aa2
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-03-13 11:33:48 -0300

    inserindo area de contato via e-mail

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const themeButton = document.getElementById('theme');
 themeButton.addEventListener('change', () => {
   document.body.classList.toggle('dark')
   document.body.classList.toggle('light')
+});
+
+const contactBtn = document.getElementById('contactBtn');
+
+contactBtn.addEventListener('click', () =>{
+  document.querySelector('.form-contact-container').classList.remove('form-hidden')
 })
