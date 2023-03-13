3596dec1c10f94d88af9f44de5ef6a3a0765f6b8
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-03-13 09:17:14 -0300

    atualizando tema claro e escuro

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const themeButton = document.getElementById('theme');
 
 themeButton.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
   document.body.classList.toggle('light')
 })
