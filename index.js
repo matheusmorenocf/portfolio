2a93714e0c5f840a7d1c0004c2d992f9df088625
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-03-16 00:18:05 -0300

    atualizaçao do header e do switcher

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-switchTheme();
 
-function switchTheme() {
+function events() {
   const themeButton = document.getElementById('theme');
 
   themeButton.addEventListener('change', () => {
     document.body.classList.toggle('dark');
     document.body.classList.toggle('light');
+
+    document.body.classList.contains('dark') ? document.getElementById('switcher').classList.add('bi-brightness-high-fill') : document.getElementById('switcher').classList.add('bi-moon-fill');
+
+    document.body.classList.contains('dark') ? document.getElementById('switcher').classList.remove('bi-moon-fill') : document.getElementById('switcher').classList.add('bi-brightness-high-fill');
+
+
+  
   });
 
   const contactBtn = document.getElementById('contactBtn');
-
   contactBtn.addEventListener('click', () => {
     document.querySelector('.form-contact-container').classList.remove('form-hidden');
   });
+
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if( currentScrollPos <= 0 || prevScrollpos > currentScrollPos ) {
+      document.querySelector('header').classList.add('visible');
+    } else{
+      document.querySelector('header').classList.remove('visible');
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+
 }
+
+
+events();
+
+
